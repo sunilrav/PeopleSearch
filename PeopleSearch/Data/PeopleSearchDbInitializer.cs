ae0c64d17285b8cee6ec5ca068d6cbd63b40090d
sunil ravulapalli <rsunilb@hotmail.com>
2016-08-05 17:03:44 -0600

    Added better seed data

--- PeopleSearch/Data/PeopleSearchDbInitializer.cs
+++ PeopleSearch/Data/PeopleSearchDbInitializer.cs
@@ -10,13 +10,32 @@ namespace PeopleSearch.Data
         {
             IList<Contact> contacts = new List<Contact>();
 
-            contacts.Add(new Contact() { FirstName = "Mickey", LastName = "Mouse", Age = 21, Picture = "NoPhoto.png", Interests = "Sports, Outdoors", Address = new Address { Address1 = "1010 S 340 W", City = "Salt lake city", State = "UT", Zip = "84111" } });
-            contacts.Add(new Contact() { FirstName = "Donald", LastName = "Duck", Age = 33, Picture = "NoPhoto.png", Interests = "Knitting", Address = new Address { Address1 = "3432 Thanos ln", City = "South Jordan", State = "UT", Zip = "84156" } });
-            contacts.Add(new Contact() { FirstName = "Minnie", LastName = "Mouse", Age = 38, Picture = "NoPhoto.png", Interests = "Extreme Sports", Address = new Address { Address1 = "190 Mill St", City = "Draper", State = "UT", Zip = "84115" } });
-            contacts.Add(new Contact() { FirstName = "Daffy", LastName = "Duck", Age = 55, Picture = "NoPhoto.png", Interests = "Swimming", Address = new Address { Address1 = "878 S 900 W", City = "North Salt lake", State = "UT", Zip = "84789" } });
-            contacts.Add(new Contact() { FirstName = "Trevor", LastName = "Smith", Age = 20, Picture = "NoPhoto.png", Interests = "Golf", Address = new Address { Address1 = "145 San Marino dr", City = "Carteret", State = "NJ", Zip = "07008" } });
-            contacts.Add(new Contact() { FirstName = "Peter", LastName = "Pan", Age = 20, Picture = "NoPhoto.png", Interests = "Flying, Golf", Address = new Address { Address1 = "4 Main st", City = "New Brunswick", State = "NJ", Zip = "07089" } });
-            contacts.Add(new Contact() { FirstName = "Sunil", LastName = "Ravulapalli", Age = 34, Picture = "NoPhoto.png", Interests = "Gaming, Sports", Address = new Address { Address1 = "900 Lion ct", City = "Austin", State = "TX", Zip = "98989" } });
+            contacts.Add(new Contact() { FirstName = "Mickey", LastName = "Mouse", Age = 21, Picture = "Mickey.jpg", Interests = "Sports, Outdoors", Address = new Address { Address1 = "1010 S 340 W", City = "Salt lake city", State = "UT", Zip = "84111" } });
+            contacts.Add(new Contact() { FirstName = "Donald", LastName = "Duck", Age = 33, Picture = "Mickey.jpg", Interests = "Knitting", Address = new Address { Address1 = "3432 Thanos ln", City = "South Jordan", State = "UT", Zip = "84156" } });
+            contacts.Add(new Contact() { FirstName = "Minnie", LastName = "Mouse", Age = 38, Picture = "Donald.jpg", Interests = "Extreme Sports", Address = new Address { Address1 = "190 Mill St", City = "Draper", State = "UT", Zip = "84115" } });
+            contacts.Add(new Contact() { FirstName = "Daffy", LastName = "Duck", Age = 55, Picture = "Donald.jpg", Interests = "Swimming", Address = new Address { Address1 = "878 S 900 W", City = "North Salt lake", State = "UT", Zip = "84789" } });
+            contacts.Add(new Contact() { FirstName = "Trevor", LastName = "Smith", Age = 20, Picture = "Mickey.jpg", Interests = "Golf", Address = new Address { Address1 = "145 San Marino dr", City = "Carteret", State = "NJ", Zip = "07008" } });
+            contacts.Add(new Contact() { FirstName = "Peter", LastName = "Pan", Age = 20, Picture = "Donald.jpg", Interests = "Flying, Golf", Address = new Address { Address1 = "4 Main st", City = "New Brunswick", State = "NJ", Zip = "07089" } });
+            contacts.Add(new Contact() { FirstName = "Sunil", LastName = "Ravulapalli", Age = 34, Picture = "Mickey.jpg", Interests = "Gaming, Sports", Address = new Address { Address1 = "900 Lion ct", City = "Austin", State = "TX", Zip = "98989" } });
+
+            contacts.Add(new Contact() { FirstName = "Micheal", LastName = "Jordan", Age = 21, Picture = "Mickey.jpg", Interests = "Sports, Outdoors", Address = new Address { Address1 = "1010 S 340 W", City = "Salt lake city", State = "UT", Zip = "84111" } });
+            contacts.Add(new Contact() { FirstName = "LeBron", LastName = "James", Age = 33, Picture = "Mickey.jpg", Interests = "Knitting", Address = new Address { Address1 = "3432 Thanos ln", City = "South Jordan", State = "UT", Zip = "84156" } });
+            contacts.Add(new Contact() { FirstName = "Kurt", LastName = "Russel", Age = 38, Picture = "Donald.jpg", Interests = "Extreme Sports", Address = new Address { Address1 = "190 Mill St", City = "Draper", State = "UT", Zip = "84115" } });
+            contacts.Add(new Contact() { FirstName = "Panda", LastName = "Express", Age = 55, Picture = "Donald.jpg", Interests = "Swimming", Address = new Address { Address1 = "878 S 900 W", City = "North Salt lake", State = "UT", Zip = "84789" } });
+            contacts.Add(new Contact() { FirstName = "Brad", LastName = "Pitt", Age = 20, Picture = "Mickey.jpg", Interests = "Golf", Address = new Address { Address1 = "145 San Marino dr", City = "Carteret", State = "NJ", Zip = "07008" } });
+            contacts.Add(new Contact() { FirstName = "Lord", LastName = "Saladin", Age = 20, Picture = "Donald.jpg", Interests = "Flying, Golf", Address = new Address { Address1 = "4 Main st", City = "New Brunswick", State = "NJ", Zip = "07089" } });
+            contacts.Add(new Contact() { FirstName = "Cayde", LastName = "Six", Age = 34, Picture = "Mickey.jpg", Interests = "Gaming, Sports", Address = new Address { Address1 = "900 Lion ct", City = "Austin", State = "TX", Zip = "98989" } });
+
+            contacts.Add(new Contact() { FirstName = "Moon", LastName = "Sun", Age = 21, Picture = "Mickey.jpg", Interests = "Sports, Outdoors", Address = new Address { Address1 = "1010 S 340 W", City = "Salt lake city", State = "UT", Zip = "84111" } });
+            contacts.Add(new Contact() { FirstName = "Peter", LastName = "Warlock", Age = 33, Picture = "Mickey.jpg", Interests = "Knitting", Address = new Address { Address1 = "3432 Thanos ln", City = "South Jordan", State = "UT", Zip = "84156" } });
+            contacts.Add(new Contact() { FirstName = "Titan", LastName = "Smith", Age = 38, Picture = "Donald.jpg", Interests = "Extreme Sports", Address = new Address { Address1 = "190 Mill St", City = "Draper", State = "UT", Zip = "84115" } });
+            contacts.Add(new Contact() { FirstName = "Fall", LastName = "Fin", Age = 55, Picture = "Donald.jpg", Interests = "Swimming", Address = new Address { Address1 = "878 S 900 W", City = "North Salt lake", State = "UT", Zip = "84789" } });
+            contacts.Add(new Contact() { FirstName = "Josh", LastName = "Pitt", Age = 20, Picture = "Mickey.jpg", Interests = "Golf", Address = new Address { Address1 = "145 San Marino dr", City = "Carteret", State = "NJ", Zip = "07008" } });
+            contacts.Add(new Contact() { FirstName = "Bryan", LastName = "Adams", Age = 20, Picture = "Donald.jpg", Interests = "Flying, Golf", Address = new Address { Address1 = "4 Main st", City = "New Brunswick", State = "NJ", Zip = "07089" } });
+            contacts.Add(new Contact() { FirstName = "Pon", LastName = "Hilaticus", Age = 34, Picture = "Mickey.jpg", Interests = "Gaming, Sports", Address = new Address { Address1 = "900 Lion ct", City = "Austin", State = "TX", Zip = "98989" } });
+
+
+
 
             foreach (Contact contact in contacts)
                 context.Contacts.Add(contact);
