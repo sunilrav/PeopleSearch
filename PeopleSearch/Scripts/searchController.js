585fd81dff99e5aec4ec6e3bc91db6025fb27933
sunil ravulapalli <rsunilb@hotmail.com>
2016-08-06 15:31:05 -0600

    Add no results message on client side

--- PeopleSearch/Scripts/searchController.js
+++ PeopleSearch/Scripts/searchController.js
@@ -6,16 +6,22 @@
 
         var onSearchComplete = function (response) {
             $scope.results = response.data;
+            if ($scope.results.length == 0)
+            {
+                $scope.noresults = "Could not find any contact with that name.";
+            }
             $scope.loading = false;
         };
 
         var onError = function (response) {
-            $scope.error = "No results found";
+            $scope.error = "System not avaliable at this time.(Exception)";
         };
 
         $scope.searchName = function (input) {
             $scope.loading = true;
             $scope.results = null;
+            $scope.noresults = null;
+            $scope.error = null;
             $http.get("/api/search?q=" + input)
                  .then(onSearchComplete, onError);
         };
